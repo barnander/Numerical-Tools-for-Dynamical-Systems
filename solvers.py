5bb8a0f99076cc2c8951061c1eb750a965f2daa2
barnander <nk21253@bristol.ac.uk>
2024-02-02 13:28:41 +0000

    changed code in solve_to such that it  preallocates np array size of solutions

--- solvers.py
+++ solvers.py
@@ -1,4 +1,6 @@
 #%% Packages
+%reload_ext autoreload
+%autoreload 2
 import numpy as np
 #%% Functions
 #one-step solvers wrapper
@@ -8,8 +10,8 @@ def solve_to(f,initial_conds, t_final, delta_max, solver = 'Euler'):
     to the specified endpoint (t_final) 
 
     Args:
-        fs (list): list of functions [f(t,x1,x2,..),g(t,x1,x2,...),...] defining system
-        initial_conds (dict): 't' points to initial time, 'x' points to a list of initial values in the state space
+        f (function): returns derivatives of system of odes as np array
+        initial_conds (dict): 't' points to initial time, 'x' points to an np array of initial values in the state space
         t_final (float): end time
         delta_max (float): max step size
         solver (string): solver used
@@ -17,34 +19,31 @@ def solve_to(f,initial_conds, t_final, delta_max, solver = 'Euler'):
     x_0 = initial_conds['x']
     t_0 = initial_conds['t']   
     ode_state = {'x_n':x_0,'t_n':t_0}
-
+    
     #choose one-step solver  
     if solver == 'Euler':
         solve_step = euler_step
     elif solver == 'RK4':
         solve_step = rk4_step
-
-    time = np.arange(t_0,t_final+ delta_max/2,delta_max)
-    x = x_0
-    for t in time[:-1]:
-        ode_state = solve_step(f,ode_state,delta_max)
-        x = np.column_stack((x,ode_state['x_n']))
-
-    return time,x
+    else:
+        raise ValueError("Unsupported solver: {}".format(solver))
     
-"""     no_timesteps = int((t_final-t_0)/delta_max)+ 1
-    time = np.zeros(no_timesteps)
+    total_time = t_final - t_0
+    no_timesteps = int(np.ceil(total_time/delta_max))
+
+    time = np.zeros(no_timesteps+1) #plus 1 so we include t0 and t_final
     time[0] = t_0
-    x = np.zeros((len(x_0),no_timesteps))
+    x = np.zeros((len(x_0),no_timesteps+1))
     x[:,0] = x_0
-    counter = 1 """
-"""     #while ode_state['t_n'] < t_final:
+    counter = 1 
+    while ode_state['t_n'] < t_final:
         #iterate through functions, computing the next value for each state variable
-        ode_state = solve_step(f,ode_state,delta_max)
+        h = min(delta_max, t_final - ode_state['t_n']) #adapts the final h to give the solution at exactly t_final
+        ode_state = solve_step(f,ode_state,h)
         time[counter] = ode_state['t_n']
         x[:,counter] = ode_state['x_n']
         counter+=1
- """
+    return time,x
  
 
 #one step solver functions
@@ -73,22 +72,4 @@ def rk4_step(f,ode_state,h):
 
 
 
-#%%
-import numpy as np
-import matplotlib.pyplot as plt
-import solvers
 
-def f_second_order(t,x):
-    x_dot = x[1]
-    y_dot = -x[0]
-    return np.array([x_dot,y_dot])
-
-
-x0 = 0
-y0 = 1
-initial_conds = {'t':0,'x':np.array([x0,y0])}
-t_final = 1
-h = 0.1
-time,x = solvers.solve_to(f_second_order,initial_conds,t_final,h)
-
-plt.plot(time,x[1])
